0e765d455f7263e1c84eb53e1d77bf81439dec7e
Kamil Matysiewicz <kamil@aexol.com>
2017-11-10 18:12:30 +0100

    Zignoruj generowane src na gicie.

--- syncano/aws-utils/src/default_bucket.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-const awsDefaultBucket = ctx => `${ctx.meta.instance}-bucket`;
-exports.awsDefaultBucket = awsDefaultBucket;
